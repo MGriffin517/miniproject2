112e8a896d7aba951199b1261eccfa8cf9b944db
MGriffin517 <73905920+MGriffin517@users.noreply.github.com>
2020-12-11 10:23:18 -0500

    Update MiniProject2_Table.c

--- MiniProject2_Table.c
+++ MiniProject2_Table.c
@@ -2,21 +2,21 @@
 
 int main()
 {
-printf("\n| "); printf(" Item Number |"); printf(" Description         |"); printf(" Discount 0-299 |");
-printf(" Discount 300-499 |"); printf(  "Discount 500+ |");
+printf("\n|"); printf(" Item Number |"); printf(" Description         |"); printf(" Discount 0-299 |");
+printf(" Discount 300-499 |"); printf(  " Discount 500+ |");
 
 
-printf("\n| ");
-printf(" 0           |"); printf(" Resistor            |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent   |");
+printf("\n|");
+printf(" 0           |"); printf(" Resistor            |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent    |");
 
-printf("\n| ");
-printf(" 1           |"); printf(" Capacitor           |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent   |");
+printf("\n|");
+printf(" 1           |"); printf(" Capacitor           |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent    |");
 
-printf("\n| ");
-printf(" 2           |"); printf(" Transistor          |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent   |");
+printf("\n|");
+printf(" 2           |"); printf(" Transistor          |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent    |");
 
-printf("\n| ");
-printf(" 3           |"); printf(" Integrated Circuits |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent   |\n\n");
+printf("\n|");
+printf(" 3           |"); printf(" Integrated Circuits |"); printf(" 2.5 Percent    |"); printf(" 5 Percent        |"); printf(" 10 Percent    |\n\n");
 
 int itemNumber;
 int itemQuantity;
